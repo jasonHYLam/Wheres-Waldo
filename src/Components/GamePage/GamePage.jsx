1f75f09f312fbf9d62e0a320fc1fef4ddc8b3483
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-04 21:20:04 +0000

    Show GamePage Image And UseState For Showing Box

--- src/Components/GamePage/GamePage.jsx
+++ src/Components/GamePage/GamePage.jsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
+import styles from './GamePage.module.css';
+
 export function GamePage() {
+
+const [ showTargetBox, setShowTargetBox ] = useState(false);
+
+function handleClick() {
+
+    showTargetBox ? setShowTargetBox(false) : setShowTargetBox(true);
+    console.log(`showTargetBox: ${showTargetBox}`)
+}
+
+
+
     return (
         <>
         <p>Fun game</p>
+        <section onClick={handleClick} className={styles.imageContainer}></section>
         </>
     )
 }
