96e020a7ed689bb1fc42601637691f921ede1244
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-04 18:00:02 +0000

    Add PageLayout And GamePage To React-Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { PageLayout } from './Components/PageLayout/PageLayout.jsx'
+import { GamePage } from './Components/GamePage/GamePage.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    // element
+    element: <PageLayout />,
     children: [
       {
-        path: 'wheres-waldo'
+        path: 'wheres-waldo',
+        element: <GamePage />,
       }
     ]
   }
 ])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
