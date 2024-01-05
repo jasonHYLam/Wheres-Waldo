b25496d4dbaaf04ec5707d2a06aa5bf1e6b9f31e
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-05 22:43:29 +0000

    Create DropDownBox Component

---
+++ src/Components/DropDownBox/DropDownBox.jsx
@@ -0,0 +1,16 @@
+import styles from './DropDownBox.module.css';
+
+export function DropDownBox() {
+    return (
+        <>
+        <section
+        className={styles.dropDownBox}
+        >
+            <p>Char 1</p>
+            <p>Char 2</p>
+            <p>Char 3</p>
+        </section>
+        
+        </>
+    )
+}
