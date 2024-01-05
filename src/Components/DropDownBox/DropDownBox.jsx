b555c7ec3dd906ffc4e77834e2ab71212097aa8d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-05 22:45:37 +0000

    Set Position Of DropDownBox At Mouse Coordinates

--- src/Components/DropDownBox/DropDownBox.jsx
+++ src/Components/DropDownBox/DropDownBox.jsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+import { GameContext } from '../GamePage/GamePage';
 import styles from './DropDownBox.module.css';
 
 export function DropDownBox() {
+
+    const {mouseCoords} = useContext(GameContext);
+
     return (
         <>
         <section
         className={styles.dropDownBox}
+        style={{
+            transform: `translate(${mouseCoords.x}px, ${mouseCoords.y}px)`
+        }}
         >
             <p>Char 1</p>
             <p>Char 2</p>
