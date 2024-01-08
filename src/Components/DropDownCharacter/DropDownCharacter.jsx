1834affb3f0e3484d7078df89a5a209bf7a3416a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-08 14:58:56 +0000

    Make FetchRequest To Access Backend With NameParam

--- src/Components/DropDownCharacter/DropDownCharacter.jsx
+++ src/Components/DropDownCharacter/DropDownCharacter.jsx
@@ -1,8 +1,15 @@
 import styles from './DropDownCharacter.module.css';
 
 export function DropDownCharacter({name, backgroundUrl=null}) {
+
+    async function clickCharacter() {
+        console.log(`Clicked ${name}`)
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/get_char/${name}`)
+        // hmm. could i fetch the data, set state, associate name with id, and then...
+    }
+
     return (
-        <div>
+        <div className={styles.dropDownCharacter} onClick={clickCharacter}>
             <img className={styles.characterImage} src={backgroundUrl} alt="" />
             <p>{name}</p>
         </div>
