f77850fcca9d0658ad2b6653b77aad8b765b3625
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-14 11:33:26 +0000

    Push For Inspection

--- src/Components/GamePage/GamePage.jsx
+++ src/Components/GamePage/GamePage.jsx
@@ -16,16 +16,8 @@ export function GamePage() {
     const [ mouseCoords, setMouseCoords ] = useState({x: 0, y: 0});
     // needed for backend verification
     const normalisedCoords = normaliseCoords(mouseCoords)
-
     const [ charactersData, setCharactersData ] = useState([{isFound: false}]);
-
-    // replace ref with state
-    const isGameWonRef = useRef(false);
-    // const isGameWon = isGameWonRef.current;
     const [ isGameWon, setIsGameWon ] = useState(false);
-
-    // const timerRef = useRef(0);
-    // const timerValue = timerRef.current;
     const [ timerValue, setTimerValue ] = useState(0)
 
     function normaliseCoords(coords) {
@@ -35,52 +27,6 @@ export function GamePage() {
         }
     }
 
-    ///////////////////////////////
-    // taken from TimerDisplay. Testing if it's bad to render GamePage this many times.
-    const startTimeValue = useRef(new Date().getTime())
-    const [ endTimeValue, setEndTimeValue] = useState(new Date().getTime());
-    const [ timeDiff, setTimeDiff ] = useState(0)
-
-
-    const [ formattedTimeDiff, setFormattedTimeDiff ] = useState({})
-
-    useEffect(() => {
-        function updateTimer() {
-
-            // if (isGameWonRef.current === false) {
-            if (!isGameWon) {
-                setEndTimeValue(new Date().getTime())
-                setTimeDiff( endTimeValue - startTimeValue.current )
-
-                // Convert the time elapsed into the corresponding time increment, and use floor to display integer. Use modulo operator to start counters from 0 after reaching 59.
-                // Display 01 -09 if value is less than 10.
-                let hours = Math.floor(timeDiff / (60 * 60 * 1000));
-                hours = (hours < 10) ? `0${hours}` : `${hours}`;
-
-                let minutes = Math.floor(timeDiff / (60 * 1000)) % 60;
-                minutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
-
-                let seconds = Math.floor(timeDiff / (1000)) % 60;
-                seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;
-
-                setFormattedTimeDiff({
-                    hours: hours,
-                    minutes: minutes,
-                    seconds: seconds,
-                })
-
-
-            }
-        }
-
-        updateTimer()
-
-    },
-    // [endTimeValue, isGameWonRef, timeDiff]
-    [endTimeValue, isGameWon, timeDiff]
-    );
-    // End of taking
-    ///////////////////////////////
 
     // useEffect hook to fetch the character data.
     useEffect(() => {
@@ -138,9 +84,7 @@ export function GamePage() {
 
     
     function toggleTargetBox() {
-
         showTargetBox ? setShowTargetBox(false) : setShowTargetBox(true);
-        // console.log(`showTargetBox: ${showTargetBox}`)
     }
 
     function handleClick(e) {
@@ -148,15 +92,13 @@ export function GamePage() {
         toggleTargetBox()
     }
 
-    // replace isGameWonRef with state variable
     return (
         <>
         <main className={styles.gamePage}>
                 {isGameWon ? <GameOverModal/> : null }
 
 
-                {/* <GameContext.Provider value={{ mouseCoords, normalisedCoords, charactersData, setCharactersData, isGameWonRef, imageDimensionsRef, timerValue }}> */}
-                <GameContext.Provider value={{ mouseCoords, normalisedCoords, charactersData, setCharactersData, isGameWon, imageDimensionsRef, timerValue, formattedTimeDiff }}>
+                <GameContext.Provider value={{ mouseCoords, normalisedCoords, charactersData, setCharactersData, isGameWon, imageDimensionsRef, timerValue, setTimerValue }}>
 
                     <GameHeader />
 
--- src/Components/TimerDisplay/TimerDisplay.jsx
+++ src/Components/TimerDisplay/TimerDisplay.jsx
@@ -3,65 +3,64 @@ import { GameContext } from "../GamePage/GamePage";
 
 export function TimerDisplay() {
 
-    // replace isGameWonfRef with state
-    // const { isGameWonRef } = useContext(GameContext)
-    const { isGameWon, formattedTimeDiff } = useContext(GameContext)
+    const { isGameWon } = useContext(GameContext)
 
-    console.log('is game over')
-    console.log(isGameWon)
+    // console.log('is game over')
+    // console.log(isGameWon)
 
     // yanked
     ////////////////
-    // const startTimeValue = useRef(new Date().getTime())
-    // const [ endTimeValue, setEndTimeValue] = useState(new Date().getTime());
-    // const [ timeDiff, setTimeDiff ] = useState(0)
+    const startTimeValue = useRef(new Date().getTime())
+    const [ endTimeValue, setEndTimeValue] = useState(new Date().getTime());
+    const [ timeDiff, setTimeDiff ] = useState(0)
 
 
-    // const [ formattedTimeDiff, setFormattedTimeDiff ] = useState({})
+    const [ formattedTimeDiff, setFormattedTimeDiff ] = useState({})
 
-    // useEffect(() => {
-    //     function updateTimer() {
+    useEffect(() => {
+        function updateTimer() {
 
-    //         // if (isGameWonRef.current === false) {
-    //         if (!isGameWon) {
-    //             setEndTimeValue(new Date().getTime())
-    //             setTimeDiff( endTimeValue - startTimeValue.current )
+            if (!isGameWon) {
+                setEndTimeValue(new Date().getTime())
+                setTimeDiff( endTimeValue - startTimeValue.current )
 
-    //             // Convert the time elapsed into the corresponding time increment, and use floor to display integer. Use modulo operator to start counters from 0 after reaching 59.
-    //             // Display 01 -09 if value is less than 10.
-    //             let hours = Math.floor(timeDiff / (60 * 60 * 1000));
-    //             hours = (hours < 10) ? `0${hours}` : `${hours}`;
+                // Convert the time elapsed into the corresponding time increment, and use floor to display integer. Use modulo operator to start counters from 0 after reaching 59.
+                // Display 01 -09 if value is less than 10.
+                let hours = Math.floor(timeDiff / (60 * 60 * 1000));
+                hours = (hours < 10) ? `0${hours}` : `${hours}`;
 
-    //             let minutes = Math.floor(timeDiff / (60 * 1000)) % 60;
-    //             minutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
+                let minutes = Math.floor(timeDiff / (60 * 1000)) % 60;
+                minutes = (minutes < 10) ? `0${minutes}` : `${minutes}`;
 
-    //             let seconds = Math.floor(timeDiff / (1000)) % 60;
-    //             seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;
+                let seconds = Math.floor(timeDiff / (1000)) % 60;
+                seconds = (seconds < 10) ? `0${seconds}` : `${seconds}`;
 
-    //             setFormattedTimeDiff({
-    //                 hours: hours,
-    //                 minutes: minutes,
-    //                 seconds: seconds,
-    //             })
+                setFormattedTimeDiff({
+                    hours: hours,
+                    minutes: minutes,
+                    seconds: seconds,
+                })
 
+            }
+            else if (isGameWon) {
+                console.log('game won hopefully now')
+                // setTimerValue here
+            }
+        }
 
-    //         }
-    //     }
+        updateTimer()
 
-    //     updateTimer()
-
-    // },
-    // // [endTimeValue, isGameWonRef, timeDiff]
-    // [endTimeValue, isGameWon, timeDiff]
-    // );
+    },
+    [endTimeValue, isGameWon, timeDiff]
+    );
     /////////////////
     // end of yanking
 
     return (
         <>
         <section>
-            {/* <p>{timeDiff}</p> */}
-            {/* <p>{timeDiffRef.current}</p> */}
+            {
+                !isGameWon ? 
             <p>
                 {formattedTimeDiff.hours} 
                 :
@@ -69,6 +68,17 @@ export function TimerDisplay() {
                 :
                 {formattedTimeDiff.seconds} 
             </p>
+            :
+            null
+
+            }
+            {/* // <p>
+            //     {formattedTimeDiff.hours} 
+            //     :
+            //     {formattedTimeDiff.minutes} 
+            //     :
+            //     {formattedTimeDiff.seconds} 
+            // </p> */}
 
         </section>
         </>
