8e1fbfe45eeeaf6a1fd1e756d94a8b206adc12f7
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-14 22:57:17 +0000

    Create CharacterInHeader Component

---
+++ src/Components/CharacterInHeader/CharacterInHeader.jsx
@@ -0,0 +1,7 @@
+export function CharacterInHeader({name, backgroundUrl}) {
+    return (
+        <>
+        <img src="" alt="" />
+        </>
+    )
+}
--- src/Components/Header/GameHeader.jsx
+++ src/Components/Header/GameHeader.jsx
@@ -1,11 +1,19 @@
 import styles from './GameHeader.module.css';
 import { TimerDisplay } from '../TimerDisplay/TimerDisplay';
 
+import abra from '../../assets/abra.jpeg'
+import dewgong from '../../assets/dewgong.png'
+import gloom from '../../assets/gloom.png'
+
 export function GameHeader() {
     return (
         <>
         <header className={styles.gameHeader}>
             <TimerDisplay />
+
+            <img src={abra} alt="" />
+
+
         </header>
         </>
     )
