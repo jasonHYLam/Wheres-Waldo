e62f4aef54cb19c1b139b37f1f89325a9b44cd3d
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-15 12:57:33 +0000

    Make Fetch Request To Submit Score On Form Submit

--- src/Components/SubmitForm/SubmitForm.jsx
+++ src/Components/SubmitForm/SubmitForm.jsx
@@ -1,16 +1,28 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { GameContext } from "../GamePage/GamePage";
 
 export function SubmitForm() {
     const {register, formState: {errors}, handleSubmit } = useForm();
 
+    const { timerValue } = useContext(GameContext)
+
     async function onSubmit(nameInput) {
-        console.log(nameInput)
+
+        const data = JSON.stringify({
+            name: nameInput.name,
+            timerValue: timerValue,
+        })
+
+        console.log(data)
+
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/submit_score`)
     }
     return (
         <>
         <form method="POST" action="" onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="name"></label>
-            <input type="text" name="name" 
+            <label htmlFor="name">Name</label>
+            <input type="text"
             {...register('name', {
                 required: 'Name is required',
             })}
