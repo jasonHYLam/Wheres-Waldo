614b87e956e78a2dc272ae46321c4282b5d33c4f
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2024-01-15 10:55:06 +0000

    Create Styles For CharacterInHeader Component

--- src/Components/CharacterInHeader/CharacterInHeader.jsx
+++ src/Components/CharacterInHeader/CharacterInHeader.jsx
@@ -1,7 +1,26 @@
+import { useContext, useEffect } from "react"
+import { GameContext } from "../GamePage/GamePage"
+
 export function CharacterInHeader({name, backgroundUrl}) {
+
+    const { charactersData } = useContext(GameContext)
+    console.log(charactersData)
+
+    // needs to check if the object corresponding to name has a true isFound, if so, display different style.
+    useEffect(() => {
+
+        const character = charactersData.find(character => {
+            return character.name === name
+        })
+        console.log(character)
+        // const isFound = character.isFound
+        console.log(name)
+        // console.log(isFound)
+        
+    })
     return (
         <>
-        <img src="" alt="" />
+        <img src={backgroundUrl} alt="" />
         </>
     )
 }
--- src/Components/Header/GameHeader.jsx
+++ src/Components/Header/GameHeader.jsx
@@ -1,19 +1,21 @@
 import styles from './GameHeader.module.css';
 import { TimerDisplay } from '../TimerDisplay/TimerDisplay';
+import { CharacterInHeader } from '../CharacterInHeader/CharacterInHeader';
 
 import abra from '../../assets/abra.jpeg'
 import dewgong from '../../assets/dewgong.png'
 import gloom from '../../assets/gloom.png'
 
+// how do i make it so that when i guess character, i grey it out? i need separate component i think
+// perhaps one that takes name and background url. 
 export function GameHeader() {
     return (
         <>
         <header className={styles.gameHeader}>
             <TimerDisplay />
-
-            <img src={abra} alt="" />
-
-
+            <CharacterInHeader name='Abra' backgroundUrl={abra} />
+            <CharacterInHeader name='Dewgong' backgroundUrl={dewgong} />
+            <CharacterInHeader name='Gloom' backgroundUrl={gloom} />
         </header>
         </>
     )
